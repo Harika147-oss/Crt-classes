9a57ec535310746bc121b4b48eab5e32d7bd609a
Harika147-oss <dubbakaharika3@gmail.com>
2026-07-17 14:50:35 +0530

    Update compoundInterest.java

--- compoundInterest.java
+++ compoundInterest.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
-
 public class CompoundInterest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Principal Amount: ");
         double principal = sc.nextDouble();
-
         System.out.print("Enter Rate of Interest: ");
         double rate = sc.nextDouble();
-
         System.out.print("Enter Time (in years): ");
         double time = sc.nextDouble();
-
         double amount = principal * Math.pow((1 + rate / 100), time);
         double compoundInterest = amount - principal;
-
         System.out.println("Amount = " + amount);
         System.out.println("Compound Interest = " + compoundInterest);
-
         sc.close();
     }
 }
